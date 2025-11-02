5629475c9073a8ec2a5253bbd6286fe99042ba6a
AKBINJ <jyp10311@naver.com>
2025-11-02 14:52:16 +0900

    202051102

---
+++ f-iot-react/src/components/PostCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+interface PostCardProps {
+
+}
+
+function PostCard() {
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
