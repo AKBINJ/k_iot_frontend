a5fa989fe8e2040d0931ec4f5f51cf11a613a591
AKBINJ <jyp10311@naver.com>
2025-11-06 14:21:31 +0900

    20251106

--- f-iot-react/src/App.tsx
+++ f-iot-react/src/App.tsx
@@ -1,8 +1,10 @@
+
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Basic from '@/pages/a_basic'; 
 import RoutePages from '@/pages/b_Route';
 import Hooks from '@/pages/c_hooks';
+import HTTP from '@/pages/d_http';
 
 import Navibar from './components/Navibar';
 import PostList from './_practices/a_basic/PostList';
@@ -10,7 +12,7 @@ import PostDetail from './components/PostDetail';
 import SearchApp from './_practices/c_hooks/SearchApp';
 import Z_Products from './pages/b_Route/Z_Products';
 import Z_ProductDetail from './pages/b_Route/Z_ProductDetail';
-import Z_Productinfo from './pages/b_Route/Z_Productinfo';
+import Z_ProductInfo from './pages/b_Route/Z_Productinfo';
 import Z_ProductReviews from './pages/b_Route/Z_ProductReviews';
 import Z_Dashboard from './pages/b_Route/Z_Dashboard';
 // 파일명 없으면 무조건! 해당 파일의 index 라는 이름의 파일을 가져옴
@@ -18,7 +20,8 @@ import Z_Dashboard from './pages/b_Route/Z_Dashboard';
 function App() {
   return (
     <>
-      <h1>Korea IoT React</h1>
+      {/* 경로와 상관없이 렌더링 */}
+      <h3>Korea IoT React</h3>
       <Navibar />
 
       {/* Routes 태그: Route를 감싸는 컴포넌트 */}
@@ -30,29 +33,26 @@ function App() {
           - 중첩된 자식 라우트 인식
         */}
         <Route path='/route/*' element={<RoutePages />} />
+        <Route path='/hooks' element={<Hooks />} />
+        <Route path='/http' element={<HTTP />} />
 
-        <Route path='/hooks' element= {<Hooks />} />
-
-        {/* practice */}
+        {/* //@ _practice 실습 코드 */}
         <Route path='/practice/post' element={<PostList />} />
         <Route path='/practice/post/:id' element={<PostDetail />} />
-
-        <Route path='/practice/search' element={<SearchApp />} />
+        <Route path='/practice/search' element={<SearchApp /> } />
 
         {/* //@ pages/b_Route - Z_실습 코드 */}
+        {/* 절대경로 */}
         <Route path='/' element={<Navigate to="/products" />} />
-        <Route path='/products' element={<Z_Products />}/>
-        <Route path='/products/:id' element={<Z_ProductDetail />} >
-          
-          {/* 중첩 라우트 */}
-          <Route path='info' element={<Z_Productinfo />}/>
-          <Route path='reviews' element={<Z_ProductReviews />}/>
-        
+        <Route path='/products' element={<Z_Products />} />
+        <Route path='/products/:id' element={<Z_ProductDetail />}>
+          {/* 중첩 라우트: 상대경로 */}
+          <Route path='info' element={<Z_ProductInfo />} />
+          <Route path='reviews' element={<Z_ProductReviews />} />
         </Route>
-        <Route path='/dashboard' element={<Z_Dashboard />}/>
-
+        <Route path='/dashboard' element={<Z_Dashboard />} />
+        
       </Routes>
-
     </>
   )
 }
