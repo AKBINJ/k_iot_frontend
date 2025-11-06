ae8ad6e2bd988a07170410968536b9540aae27bc
AKBINJ <jyp10311@naver.com>
2025-11-06 10:48:14 +0900

    20251106

---
+++ f-iot-react/src/pages/b_Route/Z_ProductReview.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Z_ProductReview() {
+  return (
+    <div>
+      <h4>사용자 리뷰</h4>
+      <ul>
+        <li>굿</li>
+        <li>굿2</li>
+        <li>굿3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Z_ProductReview
---
+++ f-iot-react/src/pages/b_Route/Z_Productinfo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Z_Productinfo() {
+  return (
+    <div>
+      <h4>제품 상세 정보</h4>
+      <ul>
+        <li>브랜드 : A Brand</li>
+        <li>가격: 100000원</li>
+        <li>제고: 10개 남음</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Z_Productinfo
