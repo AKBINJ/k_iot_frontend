8979fe608084058f29eea21673246a92d9de4cab
AKBINJ <jyp10311@naver.com>
2025-11-13 16:38:01 +0900

    20251113

--- f-iot-react/src/pages/f_style/index.tsx
+++ f-iot-react/src/pages/f_style/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import A_CSS from "./A_CSS";
 import B_Module from "./B_Module";
 import C_StyleComponents from "./C_StyleComponents";
+import D_Emothon from "./D_Emothon";
+import EmothonPractice from "./emothon-practice/EmothonPractice";
 
 function Index() {
   return (
@@ -21,6 +23,14 @@ function Index() {
 
       <ToggleSection title="3. Styled Components">
         <C_StyleComponents />
+      </ToggleSection>
+
+      <ToggleSection title="4. Emothon">
+        <D_Emothon />
+      </ToggleSection>
+
+      <ToggleSection title="5. Emothon + 반응형">
+        <EmothonPractice />
       </ToggleSection>
     </div>
   );
