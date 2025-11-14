35853bbba21f7da43e14f7491d918de43f2c740a
AKBINJ <jyp10311@naver.com>
2025-11-14 12:22:46 +0900

    20251114

---
+++ g-css-react/src/types/emotion.d.ts
@@ -0,0 +1,17 @@
+// : emotion theme값 타입 정의
+// - 반드시 types 폴더 내에 작성 >>> tsconfig.app.json의 includes 배열 값과 일치
+// emotion.d.ts
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    mode: "light" | "dark";
+    colors: {
+      background: string;
+      card: string;
+      text: string;
+      accent: string;
+      shadow: string;
+    };
+  }
+}
